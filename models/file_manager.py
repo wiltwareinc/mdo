344165fde99f4703a768069a3a95716f880b3687
wiltwareinc <wiltwareinc+github@gmail.com>
2026-01-29 21:01:17 -0500

    chore: add creation metadata, fix: various fixes

--- models/file_manager.py
+++ models/file_manager.py
@@ -2,10 +2,12 @@
 # essentially the backend with filesystems
 import json
 import logging
+import shutil
 import sys
 import time
 from datetime import datetime
 from pathlib import Path
+from typing import Optional
 from zoneinfo import ZoneInfo
 
 from typing_extensions import Any, Dict, List
@@ -23,6 +25,7 @@ class FileManager:
         self.songs = self.read_songs()
         self.albums.sort(key=lambda x: x["slug"])
         self.songs.sort(key=lambda x: x["slug"])
+        self.songs_set = [s["slug"] for s in self.songs]
 
     # READING
 
@@ -41,19 +44,6 @@ class FileManager:
                 )
                 continue
             metadata = {}
-            try:
-                with open(sd / ".metadata.json", "r") as item:
-                    metadata = json.load(item)
-            except FileNotFoundError:
-                logging.warning(f"Missing metadata for {sd}.")
-                # make metadata file...
-                self.create_metadata("song", sd)
-                # for now we just quit LOL
-                continue
-            except json.JSONDecodeError as e:
-                logging.warning(f"Bad metadata for {sd}: {e}")
-                # will need to be fixed in the future, for now just skip
-            # determine # of lyrics and projects and renders
             dlyric: Path = sd / "lyrics"
             drender: Path = sd / "renders"
             dprojects: Path = sd / "projects"
@@ -64,6 +54,19 @@ class FileManager:
                 drender.mkdir()
             if not dprojects.exists():
                 dprojects.mkdir()
+            try:
+                with open(sd / ".metadata.json", "r") as item:
+                    metadata = json.load(item)
+            except FileNotFoundError:
+                logging.warning(f"Missing metadata for {sd}.")
+                # make metadata file...
+                metadata = self.create_metadata("song", sd)
+                with open(sd / ".metadata.json", "w") as item:
+                    json.dump(metadata, item, indent=2)
+                continue
+            except json.JSONDecodeError as e:
+                logging.warning(f"Bad metadata for {sd}: {e}")
+                # will need to be fixed in the future, for now just skip
 
             songs.append(metadata)
 
@@ -102,7 +105,7 @@ class FileManager:
         return albums
 
     ### CREATION
-    # both are tempo, will be added to a utilities folder soon
+    # both are temp, will be added to a utilities folder soon
     def iso_to_timestamp(self, iso, tz: ZoneInfo):
         if len(iso) == 8 and iso.isdigit():
             y = int(iso[0:4])
@@ -112,7 +115,7 @@ class FileManager:
         return
 
     def create_metadata(self, kind: str, slug: Path):
-        metadata: Dict = {}
+        metadata = {}
         # All metadata should have a slug and title
         metadata["slug"] = f"{slug.name}"
         # Automatically generate title based on file
@@ -173,7 +176,164 @@ class FileManager:
             else current_time
         )
         metadata["modified_at"] = current_time
-        return
+        return metadata
+
+    def create_album(self, title, tracklist: list[str]) -> Optional[Path]:
+        """
+        Creates an album.
+
+        Args:
+            title: The name of the album
+            tracklist: The tracklist, an array in order.
+
+        Returns:
+            The slug of the album's root folder, as a Path, or None if it fails
+        """
+        # verify that the location is valid (item with same name was not made on same day)
+        date = datetime.now().strftime("%Y%m%d")  # YYYYMMDD
+        slug = f"{date}-{title}"
+
+        # verify it doesn't already exist
+        albums_path = self.droot / "albums"  # ? should these be self. variables?
+        for sd in albums_path.iterdir():
+            if sd.name == slug:
+                logging.error(f"{sd} is a duplicate. Not making")
+                return None
+
+        # Begin creating folders
+        proot = albums_path / slug
+        proot.mkdir()
+        songs = proot / "songs"
+        other = proot / "other"
+        songs.mkdir()
+        other.mkdir()
+
+        # now we need to check and see if the songs exist
+        for item in tracklist:
+            if item not in self.songs_set:
+                logging.warning(f"{item} is not in list of songs. Skipping")
+                continue
+            (songs / item).symlink_to(self.droot / "songs" / item)
+
+        metadata = self.create_metadata("album", proot)
+        if metadata is not None:
+            with open(proot / ".metadata.json", "w") as f:
+                json.dump(metadata, f, indent=2)
+        else:
+            logging.warning(f"Metadata for {proot} not created.")
+
+        return proot
+
+    def create_song(self, title, args: list[str]) -> Optional[Path]:
+        """
+        Creates a song and the appropriate folders.
+        
+        Args:
+            title: the name of the song
+            args: A list of arguments. "lyric" means create a lyric, "session" means create a session.
+            
+        Returns:
+            The slug of the song's root folder as a Path, or None if it fails.
+        """ 
+        date = datetime.now().strftime("%Y%m%d")  # YYYYMMDD
+        slug = f"{date}-{title}"
+
+        # verify it doesn't already exist
+        songs_path = self.droot / "songs"  # ? should these be self. variables?
+        for sd in songs_path.iterdir():
+            if sd.name == slug:
+                logging.error(f"{sd} is a duplicate. Not making")
+                return None
+                
+        sroot = songs_path / slug
+        lyrics = sroot / "lyrics"
+        renders = sroot / "renders"
+        projects = sroot / "projects"
+        sroot.mkdir()
+        lyrics.mkdir()
+        renders.mkdir()
+        projects.mkdir()
+        
+        # check args
+        if "lyrics" in args:
+            self.create_lyrics(sroot, title)
+        if "project" in args:
+            self.create_project(sroot, title)
+        
+        # build metadata
+        self.create_metadata("song", sroot)
+        return sroot
+        
+    def create_lyrics(self, root: Path, title) -> Optional[Path]:
+        """
+        Creates a lyric.
+        
+        Args:
+            root: Root of song to create a lyric in
+        Returns:
+            Path of the lyrics file
+        
+        Note: this can fail if the lyrics/ dir does not exist in the directory.
+        """
+        # verify that the lyrics dir exists
+        lyricsd: Path = root / "lyrics"
+        if not lyricsd.exists():
+            logging.error(f"{root} does not have a lyrics folder")
+            return None
+        
+        date = datetime.now().strftime("%Y%m%d")  # YYYYMMDD
+        slug = f"{date}-{title}"
+        # we can be more lenient, let's do a check
+        lyricsf = lyricsd / slug
+        candidate = lyricsf.with_suffix(".txt")
+        counter = 1
+        
+        if candidate.exists():
+            while (lyricsd / f"{slug}-{counter}.txt").exists():
+                counter += 1
+            candidate = (lyricsd / f"{slug}-{counter}.txt")
+        candidate.touch()
+            
+        return candidate
+        
+    def create_project(self, root: Path, title) -> Optional[Path]:
+        """
+        Creates a project.
+        
+        Args:
+            root: Root of song to create a project in
+        Returns:
+            Path of the project specific file (ex. a .RPP file for Reaper)
+        
+        Note: this can fail if the projects/ dir does not exist in the directory.
+        """
+        projectd: Path = root / "projects"
+        if not projectd.exists():
+            logging.error(f"{root} does not have a projects folder")
+            return None
+        
+        date = datetime.now().strftime("%Y%m%d")  # YYYYMMDD
+        slug = f"{date}-{title}"
+        # we can be more lenient
+        projectf = projectd / slug
+
+        candidate = projectf
+        counter = 1
+        
+        if candidate.exists():
+            while (projectd / f"{slug}-{counter}").exists():
+                counter += 1
+            candidate = (projectd / f"{slug}-{counter}")
+        candidate.mkdir()
+
+        # for now we are just grabbing the reaper default, will be extensible in the future
+        reaper_default = (
+            Path("~/.config/REAPER/ProjectTemplates/default.RPP").expanduser()
+        )
+        dest = candidate / f"{title}.RPP"
+        shutil.copy2(reaper_default, dest)
+
+        return dest
 
 
 # testing
