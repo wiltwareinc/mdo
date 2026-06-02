7384c3fca28de81da9ec10b6cb7987534a9de5f1
wiltwareinc <wiltwareinc+github@gmail.com>
2026-06-02 16:49:01 -0400

    feat: add React connection capability

--- app/main.py
+++ app/main.py
@@ -28,7 +28,15 @@ logging.config.dictConfig(LOGGING)
 
 from fastapi import FastAPI
 from app.api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MDO API", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
